cdf119dc335b63049b55f61891f3b405ec4adf4c
tday1 <git@tday.au>
2023-03-09 20:59:44 +1000

    Added async support and migrated to surf http client

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,5 @@
 mod push;
 
-use async_std::{
-    prelude::*,
-    task
-};
-
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error + Send + Sync>>;
 
 /// The Loki client
@@ -20,27 +15,35 @@ impl Loki {
     }
 
     pub async fn push_log(&self, stream_name: String, stream_value: String, log_messages: Vec<[String; 2]>) -> Result<()> {
-        push::push_log(self.address.clone(), stream_name, stream_value, log_messages)
+        // Wrapper for the push_log function
+        let result = push::push_log(self.address.clone(), stream_name, stream_value, log_messages)
             .await?;
-
-        Ok(())
+        
+        Ok(result)
     }
 }
-
+ 
+// Simple test to ensure that the client can push a log to Loki
 #[cfg(test)]
 mod tests {
     use super::*;
     use std::time::SystemTime;
 
-    #[test]
-    fn simple_push() {
+    #[async_std::test]
+    async fn simple_push() {
         let client = Loki::new("http://localhost:3100".to_string());
 
         let timestamp = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH)
             .unwrap()
             .as_nanos();
 
-        client.push_log("foo".to_string(), "bar".to_string(),  vec![[timestamp.to_string(), "Test log!".to_string()]]);
-        assert!(true);
+        let result = client.push_log("foo".to_string(), "bar".to_string(),  vec![[timestamp.to_string(), "Test log!".to_string()]])
+            .await;
+
+        if !result.is_ok() {
+            println!("Error: {:?}", result);
+        }
+        
+        assert_eq!(result.is_ok(), true);
     }
 }
